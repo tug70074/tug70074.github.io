0989edeb61bb9bfeb24628afe036bf46acdef3db
joelj <tug70074@temple.edu>
2020-11-01 20:46:14 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
+const { Console } = require("console");
+
 window.onload = () => {
   'use strict';
-
+  console.log("SW started");
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
              .register('./sw.js');
