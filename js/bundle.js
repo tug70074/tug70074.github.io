9ba732ffaf381c6d4e947d9c1b7e1cd2813cebd9
joelj <tug70074@temple.edu>
2020-11-07 20:20:14 -0500

    Update bundle.js

--- js/bundle.js
+++ js/bundle.js
@@ -188,8 +188,9 @@ process.umask = function() { return 0; };
 const axios = require('axios')
 
 axios
-	.get('https://www.reddit.com/r/programming.json')
+	.get('https://www.scrapingbee.com/blog/web-scraping-javascript/')
 	.then((response) => {
+		console.log("Here is the response from the Url : ")
 		console.log(response)
 	})
 	.catch((error) => {
