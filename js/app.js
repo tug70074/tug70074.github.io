6b2876da5e42448e5c06df5a31f43587a0e3ff57
joelj <tug70074@temple.edu>
2020-11-07 20:04:54 -0500

    asdf

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js')
     .then((reg) => console.log('SW registered',reg))
     .catch((err) => console.log('service worker not registered',err));
 }
-
-console.log("helloworld");
