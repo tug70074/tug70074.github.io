61b28de96e4c37716026f3bc9ebcd2e3d961d9cd
joelj <tug70074@temple.edu>
2020-11-07 20:19:37 -0500

    Update web_scraper.js

--- js/web_scraper.js
+++ js/web_scraper.js
@@ -3,6 +3,7 @@ const axios = require('axios')
 axios
 	.get('https://www.scrapingbee.com/blog/web-scraping-javascript/')
 	.then((response) => {
+		console.log("Here is the response from the Url : ")
 		console.log(response)
 	})
 	.catch((error) => {
