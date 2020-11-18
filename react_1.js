50ac8ab7bf5eb97e1b8526e7e9cc47ab71fbd2b2
joelj <tug70074@temple.edu>
2020-11-17 21:43:13 -0500

    updated feature popup react

--- react_1.js
+++ react_1.js
@@ -33,7 +33,26 @@ class FeatureContainer extends React.Component {
             <FeatureTile
             img = "/images/book.png"
             ID = {i}
-            onClick = {() => alert('click')}
+            onClick = {() => {
+                var modal = document.getElementsByClassName("myModal")[0];
+                // Get the button that opens the modal
+                var btn = document.getElementsByClassName("fTitle") 
+                // Get the <span> element that closes the modal
+                var span = document.getElementsByClassName("close")[0];
+                // When the user clicks the button, open the modal
+                modal.style.display = "block";
+                injectFunction(i);
+                // When the user clicks on <span> (x), close the modal
+                span.onclick = function() {
+                    modal.style.display = "none";
+                }
+                // When the user clicks anywhere outside of the modal, close it
+                window.onclick = function(event) {
+                    if (event.target == modal) {
+                        modal.style.display = "none";
+                        }
+                }
+            }}
             />
         );
 
@@ -78,6 +97,18 @@ class PhillyMap extends React.Component {
     }
 }
 
+function ModalWindow(){
+        return(
+            <div className="myModal modal">
+                <div className="modal-content">
+                    <span className="close">&times;</span>
+                    <div className="txtSection">Some text in the Modal..</div>
+                </div>
+            </div>
+        );
+}
+
+
 class CovidPage extends React.Component{
     render(){
         return(
@@ -85,6 +116,7 @@ class CovidPage extends React.Component{
                 <Header />
                 <PhillyMap />
                 <FeatureContainer />
+                <ModalWindow />
             </div>
         );
     }
